aea42e9b889aaddeeccb873cf3257bbc74109b58
wolfblack27 <tigowolfblack27@gmail.com>
2024-01-03 21:08:52 -0300

    finalizacao do projeto 1

---
+++ projeto01_Tributo/assets/javaScript/index.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+
+   
+    const voltaTop = document.querySelector('.volta-top');
+   
+    voltaTop.addEventListener('click',function(){
+
+        window.scrollTo(0, 0)
+       
+    });
+    
+};
+/*
+mostrar=(x)=>{
+    alert(x)
+    
+    }
+window.addEventListener('load',function(){
+
+    this.alert('Pagina carregada pela function anonima');
+    mostrar("Texto function arrow");
+});
+*/
