8583bf4fdce51110744d2834cd2a3ee5eb7ab878
wolfblack27 <tigowolfblack27@gmail.com>
2024-01-04 19:01:36 -0300

    finalizacao do projeto

--- projeto02_Form/assets/js/indexJs.js
+++ projeto02_Form/assets/js/indexJs.js
@@ -1,5 +1,11 @@
 window.onload = function(){
 
+    const form = document.querySelector('form');
+    
+    form.addEventListener('submit',function(){
+       
+    });
+    
     /*alert('aqui');
     console.log('aqui');*/
 };
