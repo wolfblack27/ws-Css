b72b4686f2e0fc9d3d9b08878ed56f774cd5e582
wolfblack27 <tigowolfblack27@gmail.com>
2024-01-05 13:38:50 -0300

    modificações no arquivo javascript

--- projeto02_Form/assets/js/indexJs.js
+++ projeto02_Form/assets/js/indexJs.js
@@ -1,24 +1,26 @@
-window.onload = function(){
-
-    const form = document.querySelector('form');
-    const btnCadastrar = document.querySelector('.btn-submit')
-    btnCadastrar.addEventListener('click',function(){
-       const titulo = form.querySelector('[name="title"]')
-       const unicoDono = form.querySelector('[name="unique_owner"]')
-      console.log(titulo.value,unicoDono.value);
-       
-      /*obtendo e pegando os valores do check*/ 
-       const opcionais = form.querySelectorAll('[name="optional[]"]')
-       for (let checkbox of opcionais) {
-        if (checkbox.checked)
-          
-          console.log(checkbox.value);
-      } 
-       
-
-       
-    });
+window.onload = function () {
+  const form = document.querySelector("form");
+  const btnCadastrar = document.querySelector(".btn-submit");
+  btnCadastrar.addEventListener("click", function () {
+    const titulo = form.querySelector('[name="title"]');
+    const preco = form.querySelector('#price');
+    const data = form.querySelector('#purchase_date');
+    const cambio = form.querySelector('[name="gear"]');
+    const unicoDono = form.querySelector('[name="unique_owner"]');
+    console.log(titulo.value); 
+    console.log(preco.value);
+    console.log(data.value);
+    console.log(unicoDono.value);
+    console.log(cambio.value);
     
-    /*alert('aqui');
+
+    /*obtendo e pegando os valores do check*/
+    const opcionais = form.querySelectorAll('[name="optional[]"]');
+    for (let checkbox of opcionais) {
+      if (checkbox.checked) console.log(checkbox.value);
+    }
+  });
+
+  /*alert('aqui');
     console.log('aqui');*/
 };
