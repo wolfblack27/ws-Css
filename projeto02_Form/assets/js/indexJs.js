f88845c5fd274371b0e7ffdbc7bb275f2e5e6157
wolfblack27 <tigowolfblack27@gmail.com>
2024-01-04 21:55:59 -0300

    criacao do javascript

--- projeto02_Form/assets/js/indexJs.js
+++ projeto02_Form/assets/js/indexJs.js
@@ -1,8 +1,21 @@
 window.onload = function(){
 
     const form = document.querySelector('form');
-    
-    form.addEventListener('submit',function(){
+    const btnCadastrar = document.querySelector('.btn-submit')
+    btnCadastrar.addEventListener('click',function(){
+       const titulo = form.querySelector('[name="title"]')
+       const unicoDono = form.querySelector('[name="unique_owner"]')
+      console.log(titulo.value,unicoDono.value);
+       
+      /*obtendo e pegando os valores do check*/ 
+       const opcionais = form.querySelectorAll('[name="optional[]"]')
+       for (let checkbox of opcionais) {
+        if (checkbox.checked)
+          
+          console.log(checkbox.value);
+      } 
+       
+
        
     });
     
