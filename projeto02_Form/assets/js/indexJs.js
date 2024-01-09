3aa70bd57c37878f83cdc6b31ac42267ec36b9b7
wolfblack27 <tigowolfblack27@gmail.com>
2024-01-09 18:05:47 -0300

    Criacao do projeto de agencia

--- projeto02_Form/assets/js/indexJs.js
+++ projeto02_Form/assets/js/indexJs.js
@@ -7,19 +7,27 @@ window.onload = function () {
     const data = form.querySelector('#purchase_date');
     const cambio = form.querySelector('[name="gear"]');
     const unicoDono = form.querySelector('[name="unique_owner"]');
+    const textaera = form.querySelector('[name="description"]');
     console.log(titulo.value); 
     console.log(preco.value);
+    console.log(textaera.value);
     console.log(data.value);
     console.log(unicoDono.value);
     console.log(cambio.value);
     
+    textaera.addEventListener('mouseover',function(){
+        //alert("certo");
+        textaera.setAttribute('rows',30);
+    });
 
     /*obtendo e pegando os valores do check*/
     const opcionais = form.querySelectorAll('[name="optional[]"]');
     for (let checkbox of opcionais) {
       if (checkbox.checked) console.log(checkbox.value);
     }
-  });
+    
+
+});
 
   /*alert('aqui');
     console.log('aqui');*/
