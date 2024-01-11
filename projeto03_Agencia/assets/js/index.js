662088558c52cdcf79bfc947cd3fc9e1865209d4
wolfblack27 <tigowolfblack27@gmail.com>
2024-01-11 18:36:35 -0300

    criacao da section de sericos

--- projeto03_Agencia/assets/js/index.js
+++ projeto03_Agencia/assets/js/index.js
@@ -1,5 +1,13 @@
 window.addEventListener('load',function(){
 
+    const banner = document.querySelector('.banner');
+    const title = banner.querySelector('.box-banner-title')
+    const p = document.createElement('p');
+    p.innerHTML = "Adicionado depois de carregado";
+    p.setAttribute('class','outra')//onde muda de cor
+    //p.style.color='red'; //mudando a cor com java
+    title.appendChild(p)
+    console.log(title);
     
 });
 
