f503ec59f69a4735055ccd451f38a90f3959e10f
wolfblack27 <tigowolfblack27@gmail.com>
2024-01-13 17:36:07 -0300

    finalização da section servicos

--- projeto03_Agencia/assets/js/index.js
+++ projeto03_Agencia/assets/js/index.js
@@ -2,12 +2,54 @@ window.addEventListener('load',function(){
 
     const banner = document.querySelector('.banner');
     const title = banner.querySelector('.box-banner-title')
+    const pai = document.querySelector('.services-container')
     const p = document.createElement('p');
     p.innerHTML = "Adicionado depois de carregado";
     p.setAttribute('class','outra')//onde muda de cor
     //p.style.color='red'; //mudando a cor com java
     title.appendChild(p)
-    console.log(title);
+    console.log(pai);
+    creator(pai);
     
+
 });
 
+
+creator = function boxCreator(pai){
+    const serviceBox = document.createElement('div');
+    serviceBox.setAttribute('class','service-box');
+    
+    const serviceTitle = document.createElement('div');
+    serviceTitle.setAttribute('class','service-title');
+    serviceTitle.setAttribute('id','design-box');
+
+    const servicesDescription = document.createElement('div')
+    servicesDescription.setAttribute('class','services-description');
+
+    const paragrafoTitle = document.createElement('p');
+    const paragrafoDes = document.createElement('p');
+    const hTitulo = document.createElement('h3');
+    hTitulo.setAttribute('id','gerenciamento-title')
+
+    paragrafoTitle.innerHTML='Marisa';
+    hTitulo.innerHTML='Veia';
+    paragrafoDes.innerHTML = "Lorem ipsum dolor sit amet consectetur"; 
+    
+
+    serviceTitle.appendChild(paragrafoTitle);
+    servicesDescription.appendChild(hTitulo);
+    servicesDescription.appendChild(paragrafoDes);
+
+    serviceBox.appendChild(serviceTitle);
+    serviceBox.appendChild(servicesDescription);
+    
+    
+    pai.appendChild(serviceBox);
+}
+
+/*
+
+
+
+
+*/
